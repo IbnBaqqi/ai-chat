01d10800b0fe340e8ccd1a92cd3395a66341085f
Abdulbaki <Salausmart132@gmail.com>
2025-10-23 20:47:40 +0300

    extract service & repository

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,10 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
-import OpenAI from 'openai';
-import z from 'zod';
+import z, { set } from 'zod';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
-
-const client = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
-});
 
 const app = express();
 app.use(express.json());
@@ -21,8 +17,6 @@ app.get('/', (req: Request, res: Response) => {
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello, World!' });
 });
-
-const conversations = new Map<string, string>();
 
 const chatSchema = z.object({
    prompt: z
@@ -43,17 +37,10 @@ app.post('/api/chat', async (req: Request, res: Response) => {
 
    try {
       const { prompt, conversationId } = req.body;
-      const response = await client.responses.create({
-         model: 'gpt-4o-mini',
-         input: prompt,
-         temperature: 0.2,
-         max_output_tokens: 100,
-         previous_response_id: conversations.get(conversationId),
-      });
 
-      conversations.set(conversationId, response.id);
+      const response = await chatService.sendMessage(prompt, conversationId);
 
-      res.json({ response: response.output_text });
+      res.json({ response: response.message });
    } catch (error) {
       res.status(500).json({ error: 'Failed to process chat request' });
    }
